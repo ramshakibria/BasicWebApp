6a66296a75fd717f06121ce221d9245aee4fbf8e
ramshakibria <rkibria64@gmail.com>
2022-07-14 15:28:24 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,33 +6,29 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
-           return "ramsha";
+            return "ramsha";
         } else if (query.contains("which of the following numbers is the largest:")) {
             for (String number : query.split(" ")) {
                 if (number.matches("\\d+")) {
-                    if(Integer.parseInt(number) > Integer.parseInt(query.split(" ")[query.split(" ").length - 1])) {
+                    if (Integer.parseInt(number) > Integer.parseInt(query.split(" ")[query.split(" ").length - 1])) {
                         return number;
                     }
                     return number;
                 }
             }
-        }
-        else if(query.contains("what is") && query.contains("plus")) {
+        } else if (query.contains("what is") && query.contains("plus")) {
             String[] numbers = query.split("plus");
             return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]) + "";
-        }
-
-
-
-        else { // TODO extend the programm here
+        } else { // TODO extend the programm here
             return "";
 
         }
+        return "";
     }
 }
