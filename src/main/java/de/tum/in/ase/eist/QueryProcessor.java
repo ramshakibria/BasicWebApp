72be90827c63a57c6f8cbef6053759e2896658c3
ramshakibria <rkibria64@gmail.com>
2022-07-14 15:27:42 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,19 +13,22 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "ramsha";
-        } else if (query.contains("what is the largest number")) {
-            return "289";
-        } else if (query.contains("what is ") && query.contains("plus")) {
-            return "1+2=3";
-        } else if (query.contains("what is 16") && query.contains("plus")) {
-            return "16+19=35";
-} else if (query.contains("what is 15") && query.contains("plus")) {
-            return "15+6=21";
-        } else if (query.contains("what is 10") && query.contains("plus")) {
-            return "10+1=11";
-        } else if (query.contains("what is 10 plus 8")){
-            return "10+8=18";
+        } else if (query.contains("which of the following numbers is the largest:")) {
+            for (String number : query.split(" ")) {
+                if (number.matches("\\d+")) {
+                    if(Integer.parseInt(number) > Integer.parseInt(query.split(" ")[query.split(" ").length - 1])) {
+                        return number;
+                    }
+                    return number;
+                }
+            }
         }
+        else if(query.contains("what is") && query.contains("plus")) {
+            String[] numbers = query.split("plus");
+            return Integer.parseInt(numbers[0]) + Integer.parseInt(numbers[1]) + "";
+        }
+
+
 
         else { // TODO extend the programm here
             return "";
