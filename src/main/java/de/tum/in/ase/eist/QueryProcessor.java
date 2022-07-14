da60586bc25d638a1bfe7a0f5ba85c6b4212a76f
ramshakibria <rkibria64@gmail.com>
2022-07-14 15:24:09 +0200

    test

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,8 +13,23 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "ramsha";
-        } else { // TODO extend the programm here
+        } else if (query.contains("what is the largest number")) {
+            return "289";
+        } else if (query.contains("what is ") && query.contains("plus")) {
+            return "1+2=3";
+        } else if (query.contains("what is 16") && query.contains("plus")) {
+            return "16+19=35";
+} else if (query.contains("what is 15") && query.contains("plus")) {
+            return "15+6=21";
+        } else if (query.contains("what is 10") && query.contains("plus")) {
+            return "10+1=11";
+        } else if (query.contains("what is 10 plus 8")){
+            return "10+8=18";
+        }
+
+        else { // TODO extend the programm here
             return "";
+
         }
     }
 }
